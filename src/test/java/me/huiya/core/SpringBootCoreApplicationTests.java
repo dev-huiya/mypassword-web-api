0af34e53a7d7fc7df335c9c7b14022683c0fd80e
dev-huiya <k_hawon@naver.com>
2022-07-27 23:27:39 +0900

    git init

---
+++ src/test/java/me/huiya/core/SpringBootCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.huiya.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
