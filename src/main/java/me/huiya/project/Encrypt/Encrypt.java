9166e952e25dcb1abb99e75dbecb7d9f44924e2d
dev-huiya <k_hawon@naver.com>
2022-10-31 02:33:12 +0900

    서버, 클라이언트간 상호 암호화 하는 유틸 메소드 추가

--- src/main/java/me/huiya/project/Encrypt/Encrypt.java
+++ src/main/java/me/huiya/project/Encrypt/Encrypt.java
@@ -1,20 +1,33 @@
 package me.huiya.project.Encrypt;
 
 import me.huiya.core.Common.Common;
+import me.huiya.core.Common.JWTManager;
 import me.huiya.core.Encrypt.AES256Util;
 import me.huiya.core.Encrypt.SHA256Util;
 import me.huiya.core.Entity.Token;
 import me.huiya.core.Repository.TokenRepository;
+import me.huiya.core.Repository.UserRepository;
+import me.huiya.project.Repository.PasswordRepository;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.beans.ConstructorProperties;
 
 public class Encrypt {
 
+    private static TokenRepository TokenRepo;
     private static String AES_IV;
 
     @Value("${core.AES-iv}")
     public void setIV(String iv) { AES_IV = iv; }
+
+    @ConstructorProperties({
+        "TokenRepository",
+    })
+    public Encrypt(
+        TokenRepository TokenRepo
+    ) {
+        this.TokenRepo = TokenRepo;
+    }
 
     public static String encrypt(String plainText, Token token) {
         return encrypt(plainText, token.getPublicKey());
@@ -32,7 +45,7 @@ public class Encrypt {
         // 퍼블릭키를 해시한 후 32바이트로 잘라내어 사용함.
         return AES256Util.encrypt(
             plainText,
-            SHA256Util.encrypt(publicKey).substring(0, 32),
+                getTokenKey(publicKey),
             AES_IV
         );
     }
@@ -44,7 +57,71 @@ public class Encrypt {
     public static String decrypt(String encryptedText, String publicKey) {
         return AES256Util.decrypt(
             encryptedText,
-            SHA256Util.encrypt(publicKey).substring(0, 32),
+                getTokenKey(publicKey),
+            AES_IV
+        );
+    }
+
+    private static String getTokenKey(String publicKey) {
+        return SHA256Util.encrypt(publicKey).substring(0, 32);
+    }
+
+    public static String serverToClient(String masterEncryptedText, String tokenStr) {
+        Token token = TokenRepo.getTokenByToken(tokenStr.replace(JWTManager.HEADER_TOKEN_KEY, ""));
+        return serverToClient(masterEncryptedText, token);
+    }
+
+    public static String serverToClient(String masterEncryptedText, Token token) {
+        String tokenKey = getTokenKey(token.getPublicKey());
+
+        return AES256Util.encrypt(
+            // 민감정보 복호화
+            AES256Util.decrypt(
+                masterEncryptedText,
+                // master key
+                AES256Util.decrypt(
+                    token.getMasterKey(),
+                    tokenKey,
+                    AES_IV
+                ),
+                AES_IV
+            ),
+            // 재 암호화용 encryptKey (클라이언트와 공유된 키 복호화
+            AES256Util.decrypt(
+                token.getEncryptKey(),
+                tokenKey,
+                AES_IV
+            ),
+            AES_IV
+        );
+    }
+
+    public static String clientToServer(String encryptedText, String tokenStr) {
+        Token token = TokenRepo.getTokenByToken(tokenStr.replace(JWTManager.HEADER_TOKEN_KEY, ""));
+        return clientToServer(encryptedText, token);
+    }
+
+    public static String clientToServer(String encryptedText, Token token) {
+        String tokenKey = getTokenKey(token.getPublicKey());
+
+        return AES256Util.encrypt(
+            // 민감정보 복호화
+            AES256Util.decrypt(
+                encryptedText,
+                // 클라이언트와 공유된 키 복호화
+                AES256Util.decrypt(
+                    token.getEncryptKey(),
+                    tokenKey,
+                    AES_IV
+                ),
+                AES_IV
+            ),
+            // 사용자의 마스터 암호화 키로 재 암호화
+            AES256Util.decrypt(
+                token.getMasterKey(),
+                tokenKey,
+                AES_IV
+            ),
             AES_IV
         );
     }
