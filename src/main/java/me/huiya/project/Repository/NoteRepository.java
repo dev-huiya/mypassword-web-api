ad7a50373d4f75c8f869beb9cb13045ca4664c66
dev-huiya <k_hawon@naver.com>
2022-10-31 07:23:40 +0900

    Note API 추가

--- src/main/java/me/huiya/project/Repository/NoteRepository.java
+++ src/main/java/me/huiya/project/Repository/NoteRepository.java
@@ -1,8 +1,13 @@
 package me.huiya.project.Repository;
 
 import me.huiya.project.Entity.Note;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NoteRepository extends JpaRepository<Note, Integer> {
-    public Note findAllById(Integer id);
+    public Note findAllByIdAndUserId(Integer id, Integer userId);
+
+    public Page<Note> findNotesByUserId(Integer userId, Pageable pageable);
 }
